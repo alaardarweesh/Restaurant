3baecb7a105cc3ed22f9e26b044d313bc74a0ef7
alaardarweesh <darweesh_alaa@yahoo.com>
2022-07-17 17:57:24 +0300

    final

--- rest.js
+++ rest.js
@@ -1,38 +1,70 @@
 'use strict';
-let allUsers=[]
-function User (foodName,type,price){
-   
-    this.foodName=foodName;
-    this.type=type;
-    this.price=price;
-    allUsers.push(this);
+let allUsers = []
+function User(foodName, type, price) {
+
+  this.foodName = foodName;
+  this.type = type;
+  this.price = price;
+
+  allUsers.push(this);
+
 }
-const one=new User("mansaf","food","5");
+User.prototype.foodIden = function () {
+  this.foodIden = Math.floor(1000 + Math.random() * 9000);
 
-
-User.prototype.printInfo=function(){
-document.write(allUsers);
-}
-
-
-User.prototype.foodID=function(){
-this.foodID=randomIntFromInterval(1000,9999);
-console.log(this.foodID);
 }
 
 
-let forms=document.getElementById("formID");
-forms.addEventListener("submit",handleSubmit);
-function handleSubmit(event){
-event.preventDefault()
-console.log(event);
-let foodName=event.target.foodnameID.value;
-let type=event.target.typeID.value;
-let price=event.target.priceID.value;
+User.prototype.printInfo = function () {
 
-const newFood=new User(foodName,type,price);
 
-newFood.printInfo();
+
+  const getTable = document.getElementById("myTable");
+  const tr0 = document.createElement('tr');
+  const td1 = document.createElement('td');
+  const td2 = document.createElement('td')
+  const td3 = document.createElement('td')
+  const td4 = document.createElement('td')
+
+  td1.textContent = this.foodIden;
+  td2.textContent = this.foodName;
+  td3.textContent = this.type;
+  td4.textContent = this.price;
+  tr0.appendChild(td1);
+  tr0.appendChild(td2);
+  tr0.appendChild(td3);
+  tr0.appendChild(td4);
+
+  getTable.appendChild(tr0);
+
+}
+
+
+
+
+for (var i = 0; i < allUsers.length; i++) {
+  allUsers[i].printInfo();
+  allUsers[i].foodIden();
+
+
+}
+
+
+
+
+
+let formsID = document.getElementById("formID");
+formsID.addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
+
+  let foodName = event.target.foodname.value;
+  let type = event.target.type.value;
+  let price = event.target.price.value;
+
+  const newFood = new User(foodName, type, price);
+  newFood.foodIden();
+  newFood.printInfo();
 }
 
 
@@ -42,12 +74,4 @@ newFood.printInfo();
 
 
 
-function randomIntFromInterval(min, max) { 
-    return Math.floor(Math.random() * (max - min + 1) + min)
-    
-  }
-  for (var i=0;i<allUsers.length;i++){
-    allUsers[i].foodID();
-    allUsers[i].printInfo();
-    
-}
+
